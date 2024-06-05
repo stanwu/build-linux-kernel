7bfb64ba8332e4b73569aa6e843e1ec88328143f
Stan Ht. Wu <stanwu.tw@gmail.com>
2024-06-05 13:52:48 +0000

    Disable interactive

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,11 +2,14 @@
 
 pwd
 #Init build environment
-sudo apt update && sudo apt install build-essential libncurses-dev bison flex libssl-dev libelf-dev fakeroot dwarves
+sudo apt -y update && sudo apt -y install build-essential libncurses-dev bison flex libssl-dev libelf-dev fakeroot dwarves
 echo "Build environment installed"
 
+echo "Downloading kernel source"
 [ ! -f linux-6.9.3.tar.xz ] && wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.9.3.tar.xz
 [ ! -f linux-6.9.3/Makefile ] && tar -xvf linux-6.9.3.tar.xz
+
+echo "Make kernel"
 cd linux-6.9.3/
 cp ../.config .
 make
--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-pwd
-#Init build environment
-sudo apt update && sudo apt install build-essential libncurses-dev bison flex libssl-dev libelf-dev fakeroot dwarves
-echo "Build environment installed"
-
+echo "Downloading kernel source"
 [ ! -f linux-6.9.3.tar.xz ] && wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.9.3.tar.xz
 [ ! -f linux-6.9.3/Makefile ] && tar -xvf linux-6.9.3.tar.xz
+
+echo "Make kernel"
 cd linux-6.9.3/
 cp ../.config .
 make
