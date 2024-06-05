41a6332831c442278d83c39036dcfdf3540e2e26
Stan Ht. Wu <stanwu.tw@gmail.com>
2024-06-05 14:01:47 +0000

    Removed auto build kernel from post-create

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,7 +4,3 @@ pwd
 #Init build environment
 sudo apt -y update && sudo apt -y install build-essential libncurses-dev bison flex libssl-dev libelf-dev fakeroot dwarves
 echo "Build environment installed"
-
-echo "Downloading kernel source"
-
-
--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "Downloading kernel source"
 [ ! -f linux-6.9.3.tar.xz ] && wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.9.3.tar.xz
-[ ! -f linux-6.9.3/Makefile ] && tar -xvf linux-6.9.3.tar.xz
+echo "Extracting kernel source"
+[ ! -f linux-6.9.3/Makefile ] && tar -xf linux-6.9.3.tar.xz
 
 echo "Make kernel"
 cd linux-6.9.3/
