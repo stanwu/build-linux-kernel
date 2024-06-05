5e9162763cd68c300c0970dfe0a7f94c44e123b5
Stan Ht. Wu <stanwu.tw@gmail.com>
2024-06-05 13:12:55 +0000

    build kernel 6.9.3 ok

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+pwd
 #Init build environment
 sudo apt update && sudo apt install build-essential libncurses-dev bison flex libssl-dev libelf-dev fakeroot dwarves
 echo "Build environment installed"
+
+[ ! -f linux-6.9.3.tar.xz ] && wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.9.3.tar.xz
+[ ! -f linux-6.9.3/Makefile ] && tar -xvf linux-6.9.3.tar.xz
+cd linux-6.9.3/
+cp ../.config .
+make
+
